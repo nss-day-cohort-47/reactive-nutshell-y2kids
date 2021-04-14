b49b09a71b35fd772ec9001f9991d85c209d170e
Joel <jrossgage@gmail.com>
2021-04-14 13:05:41 -0500

    continued work on the event list. List is now sorted.

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Event.css";
 import { useHistory } from "react-router-dom";
 
-export const EventCard = ({ event, handleShowWeather, handleDeleteEvent  }) => {
+export const EventCard = ({ event, handleShowWeather, handleDeleteEvent }) => {
 
     const history = useHistory()
     return (
-        <div className= "card">
+
+        <div className="card">
+           
             <div className="card-content">
                 <h3>Name: {event.name}</h3>
                 <h4>Date: {event.date}</h4>
@@ -15,12 +17,13 @@ export const EventCard = ({ event, handleShowWeather, handleDeleteEvent  }) => {
                 <button type="button" onClick={() => handleShowWeather(event.date, event.location)}>Show Weather</button>
 
                 <div className="card-buttons">
-                <button type="button" onClick={() => history.push(`/events/${event.id}/edit`)}>Edit</button>
-                <button type="button" onClick={() => handleDeleteEvent(event.id)}>Delete</button>
+                    <button type="button" onClick={() => history.push(`/events/${event.id}/edit`)}>Edit</button>
+                    <button type="button" onClick={() => handleDeleteEvent(event.id)}>Delete</button>
                 </div>
 
             </div>
-            
+
         </div>
+
     );
-}
+};
--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -6,19 +6,37 @@ import { getAllEvents, deleteEvent } from '../modules/EventManager'
 
 export const EventList = () => {
     const [events, setEvents] = useState([]);
+    const [mainEvent, setMain] = useState({});
     const history = useHistory();
+
 
     const getEvents = () => {
         return getAllEvents().then(eventsFromAPI => {
-            setEvents(eventsFromAPI)
+            const sortedEvents = eventsFromAPI.sort((a, b) => {
+                let da = new Date(a.date),
+                    db = new Date(b.date);
+                return da - db;
+            })
+            const firstElement = sortedEvents.shift()
+            setMain(firstElement)
+            setEvents(sortedEvents)
         });
     };
-
     const handleDeleteEvent = (id) => {
         deleteEvent(id)
-        .then(() => getEvents());
+            .then(() => getEvents());
     };
 
+    const checkEventDate = () => {
+        let currentDate = new Date();
+        let eventDate = mainEvent.date
+        console.log(currentDate);
+        console.log(eventDate)
+        
+    //  if (eventDate - currentDate === 0 )
+    }
+    checkEventDate()
+    
     // const handleShowWeather = (date, location) => {
     //     showWeather(date, location)
     //     .then(() => weatherDetail())
@@ -30,19 +48,31 @@ export const EventList = () => {
 
     return (
         <>
-        <div className="event Component">
-       <div className="container-cards">
-           {events.sort().map(event =>
-            <EventCard
-            key={event.id}
-            event={event}
-            // handleShowWeather={handleShowWeather}
-            handleDeleteEvent={handleDeleteEvent} />)}
-       </div>
-       <button type="button" onClick={() => { history.push("/events/create") }}>
-           New Event
+            <div className="event Component">
+                <div className="container-cards">
+                    <div className="main card">
+                        <h2>Name: {mainEvent.name}</h2>
+                        <h3>Date: {mainEvent.date}</h3>
+                        <h3>Location: {mainEvent.location}</h3>
+
+                        {/* <button type="button" onClick={() => handleShowWeather(mainEvent.date, mainEvent.location)}>Show Weather</button> */}
+
+                        <div className="card-buttons">
+                            <button type="button" onClick={() => history.push(`/events/${mainEvent.id}/edit`)}>Edit</button>
+                            <button type="button" onClick={() => handleDeleteEvent(mainEvent.id)}>Delete</button>
+                        </div>
+                    </div>
+                    {events.map(event =>
+                        <EventCard
+                            key={event.id}
+                            event={event}
+                            // handleShowWeather={handleShowWeather}
+                            handleDeleteEvent={handleDeleteEvent} />)}
+                </div>
+                <button type="button" onClick={() => { history.push("/events/create") }}>
+                    New Event
        </button>
-       </div>
-       </>
+            </div>
+        </>
     );
 };
