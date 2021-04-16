705568eff80d711700ebf8fc048570e39da5bc59
Bryson Goins <brysonmg@gmail.com>
2021-04-16 14:21:46 -0500

    struggling with page re-render

--- src/components/messages/privateMessages/MsgInput.js
+++ src/components/messages/privateMessages/MsgInput.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { getAllUsers } from '../../modules/UserManager'
+import { addPM, getAllPMs } from '../../modules/PrivateMessageManager'
+import { useHistory } from 'react-router-dom'
+
 
 export const MsgInput = () => {
     const [users, setUsers] = useState([])
+    const [messages, setMessages] = useState([])
+    const [messageObj, setMessageObj] = useState({
+        userId: 0,
+        receiverId: 0,
+        message: ""
+    })
+    const history = useHistory()
+
     const currentUserId = sessionStorage.getItem("nutshell_user")
 
     const getUsers = () => {
@@ -11,22 +22,60 @@ export const MsgInput = () => {
         })
     }
 
+    const handleReceiverChange = (event) => {
+        const newMessageObj = { ...messageObj }
+        let receiverId = parseInt(event.target.value)
+        const userId = parseInt(sessionStorage.getItem("nutshell_user"))
+        newMessageObj.receiverId = receiverId
+        newMessageObj.userId = userId
+        setMessageObj(newMessageObj)
+    }
+
+    const handleMessageChange = (event) => {
+        const newMessageObj = { ...messageObj }
+        const message = event.target.value;
+
+        newMessageObj.message = message
+        setMessageObj(newMessageObj)
+    }
+
+    const getPMs = () => {
+        return getAllPMs()
+        .then(allPMs => {
+            setMessages(allPMs)
+        })
+    }
+
+    const handleClickSend = (event) => {
+        event.preventDefault()
+
+        if (messageObj.userId === 0 || messageObj.receiverId === 0) {
+            alert("Please choose a user to send a message to.")
+        } else {
+            addPM(messageObj).then(() => {
+                getPMs()
+            })
+        }
+    }
+
+
+
     useEffect(() => {
         getUsers()
-    },[])
+    }, [])
 
     return (
         <div className="inputContainer">
-            <select name="userDropdown" id="userDropdown">
-                <option value="default" disabled >Select a User</option>
+            <select name="userDropdown" id="userDropdown" onChange={handleReceiverChange} >
+                <option value="default" selected disabled >Select a User</option>
                 {users.map(user => {
                     if (user.id !== parseInt(currentUserId)) {
-                        return <option key={user.id} value={user.name}>{user.name}</option>
+                        return <option key={user.id} value={user.id}>{user.name}</option>
                     }
                 })}
             </select>
-            <textarea id="PMinputField" name="PMinputField" rows="5" cols="50" />
-            <button>Send</button>
+            <textarea id="PMinputField" name="PMinputField" rows="5" cols="50" onChange={handleMessageChange} />
+            <button onClick={handleClickSend} >Send</button>
         </div>
     )
 }
